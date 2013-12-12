f98a584e65ae70296d65dfcb4054c0792559a7d9
Ryan Clark <ryan.clark.j@gmail.com>
2013-12-11 17:46:47 -0700

    skeleton for a layer to get data and a server to provide it

--- client/index.js
+++ client/index.js
@@ -3,4 +3,8 @@ var express = require('express'),
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/data/:zoom', function (req, res, next) {
+
+});
+
 app.listen(3000);
--- client/public/script/demo.js
+++ client/public/script/demo.js
@@ -1,10 +1,47 @@
 (function() {
   this.cacheDemo = {};
 
+  var ClusterCache = L.GeoJSON.extend({
+    initialize: function (options) {
+      var self = this;
+      
+      L.GeoJSON.prototype.initialize.call(this, null, options);
+      
+      d3.json('url/for/cached-cluster.json', function (err, json) {
+        self._data = json;
+        self.zoomEnd();
+      });
+    },
+    
+    onAdd: function (map) {
+      L.LayerGroup.prototype.onAdd.call(this, map);
+      map.on('zoomend', this.zoomEnd, this);
+      if (this._data) { this.zoomEnd(); }
+    },
+    
+    onRemove: function (map) {
+      map.off('zoomend', this.zoomEnd, this);
+      L.LayerGroup.prototype.onRemove.call(this, map);
+    },
+    
+    zoomEnd: function (evt) {
+      if (this._data && this._map) {
+        var zoom = this._map.getZoom();
+        
+        this.clearLayers();
+        this.addData(this._data[zoom]);
+      }
+    }
+  });
+
+  var clusterCache = this.cacheDemo.clusterCache = function (options) {
+    return new ClusterCache(options);
+  };
+
   var map = this.cacheDemo.map = L.map('map').setView([40.346, -97.448], 5);
-  L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png',{
-      subdomains: '1234',
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Tiles &copy; <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" />'
-    }).addTo(map);
+  L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+    subdomains: '1234',
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Tiles &copy; <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" />'
+  }).addTo(map);
 
 }).call(this);

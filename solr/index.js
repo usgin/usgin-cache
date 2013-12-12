3eed8c188c0ea0c5c8b8bc82b338b6a09401351a
Ryan Clark <ryan.clark.j@gmail.com>
2013-12-12 16:17:11 -0700

    functions for adding/purging the solr index

---
+++ solr/index.js
@@ -0,0 +1,29 @@
+var solr = require('solr-client'),
+    _ = require('underscore'),
+    client = solr.createClient();
+
+client.autoCommit = true;
+
+module.exports = function (featureConfig) {
+  var feature = require('./features')(featureConfig);
+
+  return {
+    addToIndex: function (mappingName, callback) {
+      callback = callback || function () {};
+      feature.db.view('usgin-features', mappingName, function (err, response) {
+        if (err) return callback(err);
+        
+        var docs = response.rows.map(function (row) {
+          return row.value;
+        });
+
+        client.add(docs, callback);
+      });
+    },
+
+    purgeIndex: function (callback) {
+      callback = callback || function () {};
+      client.delete('id', '*', callback);
+    }
+  };
+}

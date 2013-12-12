cae1454ccc7904e2a08a2a5f605da6e058e14b8c
Ryan Clark <ryan.clark.j@gmail.com>
2013-12-11 17:30:03 -0700

    vanilla starter map

--- client/public/script/demo.js
+++ client/public/script/demo.js
@@ -1,7 +1,10 @@
 (function() {
   this.cacheDemo = {};
 
-  var map = this.cacheDemo.map = L.map('map');
-  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+  var map = this.cacheDemo.map = L.map('map').setView([40.346, -97.448], 5);
+  L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png',{
+      subdomains: '1234',
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Tiles &copy; <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" />'
+    }).addTo(map);
 
 }).call(this);

348cc8a117ed66e4c0a152380c266089483a0e6c
Ryan Clark <ryan.clark.j@gmail.com>
2013-12-12 20:52:50 -0700

    adds limited support for bbox queries. displays data

--- client/index.js
+++ client/index.js
@@ -1,10 +1,55 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    solrClient = require('solr-client'),
+    solr = solrClient.createClient();
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/data/:zoom', function (req, res, next) {
+  // ## Getting Data
+  // This function is the primary API for drawing data out of the system for visualization.
+  // All you have to do is make a GET request, and make sure to pass an integer Zoom number.
+  // Optionally, you can provide a `?bbox=w,s,e,n` query to limit the search by area.
 
+  // TODO:
+  // - Get clustered data based on the requested Zoom level
+  // - Allow for more complex searching (this may occur through a broader API)
+
+  // Make sure that a Zoom level was specified
+  if (isNaN(req.params.zoom)) return res.send(400);
+
+  // Start building a Solr Query
+  var query = solr.createQuery()
+    .q('*.*').rows(10000000);
+
+  // Deal with the bbox if provided
+  if (req.query.bbox) {
+    var bbox = req.query.bbox.split(',');
+    query = query.rangeFilter({
+      field: 'geo',
+      start: [bbox[1],bbox[0]].join(','),
+      end: [bbox[3],bbox[2]].join(',')
+    });
+  }
+
+  // Make the query
+  solr.search(query, function (err, result) {
+    if (err) return next(err);
+
+    // Convert to GeoJSON FeatureCollection
+    var features = result.response.docs.map(function (doc) {
+      var geo = doc.geo[0].split(' ');
+      return {
+        type: "Feature",
+        properties: doc,
+        geometry: {
+          type: "Point",
+          coordinates: [Number(geo[0]), Number(geo[1])]
+        }
+      };
+    });
+    res.json({type: "FeatureCollection", features: features});
+  });
 });
 
 app.listen(3000);
--- client/public/script/demo.js
+++ client/public/script/demo.js
@@ -1,47 +1,56 @@
 (function() {
   this.cacheDemo = {};
 
-  var ClusterCache = L.GeoJSON.extend({
+  var NgdsCache = L.GeoJSON.extend({
     initialize: function (options) {
       var self = this;
       
       L.GeoJSON.prototype.initialize.call(this, null, options);
-      
-      d3.json('url/for/cached-cluster.json', function (err, json) {
-        self._data = json;
-        self.zoomEnd();
-      });
     },
     
     onAdd: function (map) {
       L.LayerGroup.prototype.onAdd.call(this, map);
-      map.on('zoomend', this.zoomEnd, this);
-      if (this._data) { this.zoomEnd(); }
+      map.on('moveend', this.getData, this);
+      this.getData();
     },
     
     onRemove: function (map) {
-      map.off('zoomend', this.zoomEnd, this);
+      map.off('moveend', this.getData, this);
       L.LayerGroup.prototype.onRemove.call(this, map);
     },
     
-    zoomEnd: function (evt) {
-      if (this._data && this._map) {
-        var zoom = this._map.getZoom();
-        
-        this.clearLayers();
-        this.addData(this._data[zoom]);
+    getData: function (evt) {
+      if (this._map) {
+        var zoom = this._map.getZoom(),
+            bounds = this._map.getBounds().toBBoxString();
+        d3.json('/data/' + zoom + '?bbox=' + bounds, L.bind(function (err, data) {
+          this.clearLayers();
+          this.addData(data);
+          console.log("Drew " + data.features.length + " features");
+        }, this));
       }
     }
   });
 
-  var clusterCache = this.cacheDemo.clusterCache = function (options) {
-    return new ClusterCache(options);
+  var ngdsCache = this.cacheDemo.ngdsCache = function (options) {
+    return new NgdsCache(options);
   };
 
   var map = this.cacheDemo.map = L.map('map').setView([40.346, -97.448], 5);
   L.tileLayer('http://otile{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
     subdomains: '1234',
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Tiles &copy; <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" />'
+  }).addTo(map);
+
+  ngdsCache({
+    pointToLayer: function (f, ll) {
+      return L.circleMarker(ll, {
+        weight: 2,
+        radius: 6,
+        fillOpacity: 0.7,
+        fillColor: '#9EADE8'
+      });
+    }
   }).addTo(map);
 
 }).call(this);

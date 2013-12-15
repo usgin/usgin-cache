3413aab3e8366a0fd2647104f327c7825920c279
Ryan Clark <ryan.clark.j@gmail.com>
2013-12-15 13:01:00 -0700

    somewhat better error handling during conversion to postgis

--- features/toPostGis.js
+++ features/toPostGis.js
@@ -2,20 +2,20 @@ var spawn = require('child_process').spawn;
 
 module.exports = function toPostGis(callback) {
   var params = [
-      "-f", "PostGIS", 
-      "-skipfailures", "-append",
+      '-f', 'PostGIS', 
+      '-skipfailures', '-append',
       'PG:dbname=ngds',
-      "/vsistdin/"
+      '/vsistdin/'
     ],
-    ogr = spawn("ogr2ogr", params);
+    ogr = spawn('ogr2ogr', params);
 
-  ogr.on('close', function (code) {
-    callback(null);
-  });
-
+  var err = '';
   ogr.stderr.on('data', function (data) {
-    // This is usually the result of bad data fed into the converter.
-    callback(data);
+    err += data;
+  });
+  
+  ogr.on('close', function (code) {
+    callback(err === '' ? null : err);
   });
   
   return ogr.stdin;

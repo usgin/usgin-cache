5dca678df1192eed0cfa9620a543baa45f95003e
Ryan Clark <ryan.clark.j@gmail.com>
2013-12-16 10:29:32 -0700

    simplify the clustering query

--- cluster/pgCluster.js
+++ cluster/pgCluster.js
@@ -13,13 +13,13 @@ module.exports = function (mapping, bbox, callback) {
     console.time('query');
 
     var qs = 'SELECT kmeans, count(*), ';
-    qs += 'st_asgeojson(st_transform(st_centroid(st_convexhull(st_collect(proj_geom))), 4326)) AS centroid, ';
-    qs += 'st_asgeojson(st_transform(st_convexhull(st_collect(proj_geom)), 4326)) as poly ';
-    qs += 'FROM ( SELECT kmeans(array[ST_X(proj_geom), ST_Y(proj_geom)], ' + numberOfPoints + ') over (), proj_geom ';
-    qs += 'FROM ( SELECT st_transform(st_setsrid(geom, 4326), 3857) as proj_geom FROM ' + mapping.toLowerCase() + ' ';
+    qs += 'st_asgeojson(st_centroid(st_collect(geom))) AS centroid ';
+    // qs += 'st_asgeojson(st_convexhull(st_collect(proj_geom))) as poly'
+    qs += 'FROM ( SELECT kmeans(array[ST_X(geom), ST_Y(geom)], ' + numberOfPoints + ') over (), geom ';
+    qs += 'FROM ' + mapping.toLowerCase() + ' ';
     qs += 'WHERE ' + mapping.toLowerCase() + '.geom && st_makeenvelope(' + bbox + ', 4326) '
-    qs += ') as projected ) AS ksub ';
-    qs += 'GROUP BY kmeans ORDER BY kmeans;';
+    qs += ') AS ksub ';
+    qs += 'GROUP BY kmeans;';
 
     console.log(qs);
 
@@ -27,19 +27,21 @@ module.exports = function (mapping, bbox, callback) {
       console.timeEnd('query');
       if (err) return callback(err);
 
-      var centroids = {type: "FeatureCollection", features: []},
-          polygons = {type: "FeatureCollection", features: []};
+      function toGeoJSON(makePolys) {
+        return {
+          type: "FeatureCollection", 
+          features: result.rows.map(function (row) {
+            return {
+              type: "Feature", 
+              properties: {count: row.count},
+              geometry: JSON.parse(makePolys ? row.poly : row.centroid)
+            };
+          })
+        };
+      }
 
-      result.rows.forEach(function (row) {
-        var center = {type: "Feature", properties: {count: row.count}},
-            poly = _.clone(center);
-        center.geometry = JSON.parse(row.centroid);
-        centroids.features.push(center);
-        poly.geometry = JSON.parse(row.poly);
-        polygons.features.push(poly);
-      });
-
-      callback(null, centroids, polygons);
+      callback(null, toGeoJSON(), {});
+      // callback(null, toGeoJSON(), toGeoJSON(true));
       client.end();
     });
   });

5620d0918b99b9a11f0d79cee54398d7a6d9ea37
Ryan Clark <ryan.clark.j@gmail.com>
2013-12-19 15:35:44 -0700

    build client to exclude clusters, mix tiles and vectors

--- client/index.js
+++ client/index.js
@@ -29,8 +29,10 @@ app.get('/data/:zoom', function (req, res, next) {
   // All you have to do is make a GET request, and make sure to pass an integer Zoom number.
   // Optionally, you can provide a `?bbox=w,s,e,n` query to limit the search by area.
 
+  // ## Important
+  // This only returns features if there will be less than 3000 in the response. Otherwise it returns an empty FeatureCollection.
+
   // TODO:
-  // - Get clustered data based on the requested Zoom level
   // - Allow for more complex searching (this may occur through a broader API)
 
   // Make sure that a Zoom level was specified
@@ -47,44 +49,32 @@ app.get('/data/:zoom', function (req, res, next) {
         .q('*.*').rows(0)
         .rangeFilter(range);
 
-  if (req.params.zoom < 9) {
-    // Make a request for clusters from CouchDB
-    features.getClusters(req.params.zoom, function (err, result) {
-      if (err) return next(err);
-      res.json(result);
-    });
-  } else {
-    // Check how many features Solr would return directly
-    solr.search(query, function (err, result) {
-      if (result.response.numFound > 3000) {
-        // Get clusters dynamically from PostGIS
-        var getBboxData = require('../cluster/pgCluster');
-        getBboxData('boreholeTemperature', req.query.bbox, argv.postgresql, 30, function (err, centers, polys) {
-          if (err) return next(err);
-          res.json(centers);
+  // Check how many features Solr would return directly
+  solr.search(query, function (err, result) {
+    console.log(bbox + ': ' + result.response.numFound);
+    if (result.response.numFound < 3000) {
+      query = solr.createQuery()
+        .q('*.*').rows(result.response.numFound)
+        .rangeFilter(range);
+      solr.search(query, function (err, result) {
+        // Convert to GeoJSON FeatureCollection
+        var features = result.response.docs.map(function (doc) {
+          var geo = doc.geo[0].split(' ');
+          return {
+            type: "Feature",
+            properties: doc,
+            geometry: {
+              type: "Point",
+              coordinates: [Number(geo[0]), Number(geo[1])]
+            }
+          };
         });
-      } else {
-        query = solr.createQuery()
-          .q('*.*').rows(result.response.numFound)
-          .rangeFilter(range);
-        solr.search(query, function (err, result) {
-          // Convert to GeoJSON FeatureCollection
-          var features = result.response.docs.map(function (doc) {
-            var geo = doc.geo[0].split(' ');
-            return {
-              type: "Feature",
-              properties: doc,
-              geometry: {
-                type: "Point",
-                coordinates: [Number(geo[0]), Number(geo[1])]
-              }
-            };
-          });
-          res.json({type: "FeatureCollection", features: features});
-        });
-      }
-    });
-  }
+        res.json({type: "FeatureCollection", features: features});
+      });
+    } else {
+      res.json({type: "FeatureCollection", features: []});
+    }
+  });
 });
 
 app.listen(3000);
